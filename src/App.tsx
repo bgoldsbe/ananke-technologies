9434d485ed06c6226d33b5020bd56ddccf63b0d7
bgoldsberry <bgoldsberry@paypal.com>
2021-08-11 23:22:15 -0500

    Initial skeleton layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,80 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-logo">Ananke Technologies</h1>  
       </header>
+      <div className="App-layout">
+        <nav className="App-navigation">
+          <div className="Nav-scoreboard"/>
+          <div className="Nav-links">
+            <div className="Nav-link" /> 
+            <div className="Nav-link" /> 
+            <div className="Nav-link" /> 
+            <div className="Nav-link" /> 
+            <div className="Nav-link" /> 
+          </div>
+        </nav>
+        <div className="App-content">
+          <div className="Content-filters">
+            <div className="Content-filter" /> 
+            <div className="Content-filter" /> 
+            <div className="Content-filter" /> 
+            <div className="Content-filter" /> 
+          </div>
+          <div className="Content-container">
+            <div className="Content-table">
+              <div className="Table-header" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+            </div>
+            
+            <div className="Content-table">
+              <div className="Table-header" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+            </div>
+            
+            <div className="Content-table">
+              <div className="Table-header" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+              <div className="Table-row" /> 
+            </div>
+          </div> 
+        </div>
+      </div>
     </div>
   );
 }
